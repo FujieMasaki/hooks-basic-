825a5470d5df7b74efcdbba68318b7182ed150a0
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-04-10 12:12:55 +0900

    クリーンアップ関数を必要とするとき

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import Counter from './components/Counter';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import "./App.css";
 
 function App() {
+  const [isDisplay, setIsDisplay] = useState(true);
+  const handleDisplay = () => {
+    setIsDisplay(!isDisplay);
+    // isDisplayの表示を変える処理
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-      <Counter />
+        <h2>useEffect</h2>
+        <button onClick={() => handleDisplay()}>
+          {isDisplay ? "コンポーネント非表示" : "コンポーネント表示"}
+        </button>
+        {isDisplay && <Counter />}
       </header>
     </div>
   );
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter: React.VFC = () => {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("mount時と特定の値が変更した時に表示");
+    const countUp = setInterval(() => {
+      setCount1((prevCount) => prevCount + 1);
+      console.log('countUp')
+  }, 1000);
+  // 非同期処理やsubscription処理を行なった場合はクリーンアップする
+  return ()=>{
+    clearInterval(countUp)
+    console.log('unmount')
+  }
+}, []);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
   return (
     <>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>ボタン</button>
-      <button onClick={() => setCount(0)}>リセット</button>
+    <div>
+      <span>カウント1</span>
+      <button onClick={() => setCount1(count1 + 1)}></button>
+      {count1}
+    </div>
+
+    <div>
+      <span>カウント2</span>
+      <button onClick={() => setCount2(count2 + 2)}></button>
+      {count2}
+
+    </div>
+
     </>
   );
 };
