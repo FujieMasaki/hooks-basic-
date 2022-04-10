591f1f7d272300d6019f9adf2a4168f68ad6a9a7
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-04-10 09:18:28 +0900

    first comitt

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+
+const Counter: React.VFC = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>ボタン</button>
+      <button onClick={() => setCount(0)}>リセット</button>
+    </>
+  );
+};
+
+export default Counter;
